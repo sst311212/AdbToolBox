411992c9e2807202fccfbf50546b3e91f8b555af
sst311212 <sst311212@gmail.com>
2016-05-20 17:38:37 +0800

    vs2012

---
+++ AdbToolBox/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace AdbToolBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ADBTick = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 136);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // ADBTick
+            // 
+            this.ADBTick.Enabled = true;
+            this.ADBTick.Interval = 1000;
+            this.ADBTick.Tick += new System.EventHandler(this.ADBTick_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(16, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bootloader";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(109, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(16, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Recovery";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(109, 50);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Reboot";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 88);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reboot";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Devices:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(231, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(224, 136);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Device Info:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(6, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(6, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(6, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(6, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Location = new System.Drawing.Point(231, 177);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(224, 88);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Flash";
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(126, 50);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Sideload";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(26, 50);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Zip";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(126, 21);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Recovery";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(26, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Boot";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(314, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(141, 12);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Author: Ahuangzai (阿皇仔)";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 280);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "ADB Tool Box";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer ADBTick;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ AdbToolBox/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AdbToolBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(StartADB);
+            worker.RunWorkerAsync();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://ahzol.com");
+        }
+        
+        public void StartADB(object sender, DoWorkEventArgs e)
+        {
+            Process adb = new MyProcess("adb.exe", "start-server");
+            adb.Start();
+            ADBTick.Start();
+        }
+
+        private void DetectDevices_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<string> devices = new List<string>();
+            Process adb = new MyProcess("adb.exe", "devices");
+            adb.Start();
+            adb.StandardOutput.ReadLine();
+            while (true)
+            {
+                string i = adb.StandardOutput.ReadLine();
+                if (i != "")
+                    devices.Add(i);
+                else
+                    break;
+            }
+            Process fst = new MyProcess("fastboot.exe", "devices");
+            fst.Start();
+            while (true)
+            {
+                string i = fst.StandardError.ReadLine();
+                if (i == "" || i == null)
+                    continue;
+                if (i.Contains("htc_"))
+                    break;
+                devices.Add(i);
+            }
+            e.Result = devices;
+        }
+
+        private void DetectDevices_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            List<string> devices = e.Result as List<string>;
+            foreach (string i in devices)
+                if (listBox1.Items.Contains(i) == false)
+                    listBox1.Items.Add(i);
+            for (int i = 0; i < listBox1.Items.Count; i++)
+                if (devices.Contains(listBox1.Items[i]) == false)
+                    listBox1.Items.Remove(listBox1.Items[i]);
+        }
+
+        private void ADBTick_Tick(object sender, EventArgs e)
+        {
+            BackgroundWorker thread = new BackgroundWorker();
+            thread.DoWork += new DoWorkEventHandler(DetectDevices_DoWork);
+            thread.RunWorkerCompleted += new RunWorkerCompletedEventHandler(DetectDevices_RunWorkerCompleted);
+            thread.RunWorkerAsync(thread);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string device = listBox1.SelectedItem as string;
+            if (device == null || device == "")
+            {
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+            }
+            else if (device.Contains("fastboot"))
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button3.Text = "rebootRUU";
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = false;
+                device = device.Split('\t')[0];
+                Process fst = new MyProcess("fastboot.exe");
+                fst.StartInfo.Arguments = "-s " + device + " getvar all";
+                fst.Start();
+                string i = fst.StandardError.ReadToEnd();
+                if (i.Contains("product"))
+                    label2.Text = "Product：" + i.Substring(i.IndexOf("product")).Split(' ')[1].Split('\n')[0];
+                if (i.Contains("version-main"))
+                    label3.Text = "Version：" + i.Substring(i.IndexOf("version-main")).Split(' ')[1].Split('\n')[0];
+                if (i.Contains("mid"))
+                    label4.Text = "MID：" + i.Substring(i.IndexOf("mid")).Split(' ')[1].Split('\n')[0];
+                if (i.Contains("cid"))
+                    label5.Text = "CID：" + i.Substring(i.IndexOf("cid")).Split(' ')[1].Split('\n')[0];
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button3.Text = "Recovery";
+                button4.Enabled = true;
+                if (device.Contains("sideload"))
+                    button8.Enabled = true;
+                device = device.Split('\t')[0];
+                Process adb = new MyProcess("adb.exe");
+                adb.StartInfo.Arguments = "-s " + device + " shell getprop";
+                adb.Start();
+                string i = adb.StandardOutput.ReadToEnd();
+                if (i.Contains("ro.product.manufacturer"))
+                    label2.Text = "Manufacturer：" + i.Substring(i.IndexOf("ro.product.manufacturer")).Split('[')[1].Split(']')[0];
+                if (i.Contains("ro.product.model"))
+                    label3.Text = "Model：" + i.Substring(i.IndexOf("ro.product.model")).Split('[')[1].Split(']')[0];
+                if (i.Contains("ro.product.version"))
+                    label4.Text = "Version：" + i.Substring(i.IndexOf("ro.product.version")).Split('[')[1].Split(']')[0];
+                if (i.Contains("ro.mid"))
+                    label5.Text = "MID：" + i.Substring(i.IndexOf("ro.mid")).Split('[')[1].Split(']')[0];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string device = listBox1.SelectedItem as string;
+            if (device.Contains("fastboot"))
+            {
+                device = device.Split('\t')[0];
+                Process fst = new MyProcess("fastboot.exe");
+                fst.StartInfo.Arguments = "-s " + device + " reboot-bootloader";
+                fst.Start();
+            }
+            else
+            {
+                device = device.Split('\t')[0];
+                Process adb = new MyProcess("adb.exe");
+                adb.StartInfo.Arguments = "-s " + device + " reboot bootloader";
+                adb.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string device = listBox1.SelectedItem as string;
+            if (device.Contains("fastboot"))
+            {
+                device = device.Split('\t')[0];
+                Process fst = new MyProcess("fastboot.exe");
+                fst.StartInfo.Arguments = "-s " + device + " oem reboot-download";
+                fst.Start();
+            }
+            else
+            {
+                device = device.Split('\t')[0];
+                Process adb = new MyProcess("adb.exe");
+                adb.StartInfo.Arguments = "-s " + device + " reboot download";
+                adb.Start();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string device = listBox1.SelectedItem as string;
+            if (device.Contains("fastboot"))
+            {
+                device = device.Split('\t')[0];
+                Process fst = new MyProcess("fastboot.exe");
+                fst.StartInfo.Arguments = "-s " + device + " oem rebootRUU";
+                fst.Start();
+            }
+            else
+            {
+                device = device.Split('\t')[0];
+                Process adb = new MyProcess("adb.exe");
+                adb.StartInfo.Arguments = "-s " + device + " reboot recovery";
+                adb.Start();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string device = listBox1.SelectedItem as string;
+            if (device.Contains("fastboot"))
+            {
+                device = device.Split('\t')[0];
+                Process fst = new MyProcess("fastboot.exe");
+                fst.StartInfo.Arguments = "-s " + device + " reboot";
+                fst.Start();
+            }
+            else
+            {
+                device = device.Split('\t')[0];
+                Process adb = new MyProcess("adb.exe");
+                adb.StartInfo.Arguments = "-s " + device + " reboot";
+                adb.Start();
+            }
+        }
+
+        public void FlashAsyncWorker(object sender, DoWorkEventArgs e)
+        {
+            List<string> para = e.Argument as List<string>;
+            Process p = new Process();
+            p.StartInfo.FileName = para[0];
+            p.StartInfo.Arguments = para[1];
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+        }
+
+        public void FlashSomeThing(string partition)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "All Files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                List<string> Para = new List<string>();
+                if (partition == "sideload")
+                {
+                    Para.Add("adb.exe");
+                    Para.Add("sideload " + dialog.FileName);
+                }
+                else
+                {
+                    Para.Add("fastboot.exe");
+                    Para.Add("flash " + partition + " " + dialog.FileName);
+                }
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.DoWork += new DoWorkEventHandler(FlashAsyncWorker);
+                worker.RunWorkerAsync(Para);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FlashSomeThing("boot");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FlashSomeThing("recovery");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FlashSomeThing("zip");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FlashSomeThing("sideload");
+        }
+    }
+
+    public partial class MyProcess : Process
+    {
+        public MyProcess(string FileName = null, string Arguments = null)
+        {
+            this.StartInfo.CreateNoWindow = true;
+            this.StartInfo.RedirectStandardError = true;
+            this.StartInfo.RedirectStandardOutput = true;
+            this.StartInfo.UseShellExecute = false;
+            this.StartInfo.FileName = FileName;
+            this.StartInfo.Arguments = Arguments;
+        }
+    }
+}
